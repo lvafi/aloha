0c77f1ba3497c112dbfd89a9e3add9398dcd7224
vf <l_vafi@yahoo.com>
2017-06-05 23:42:33 -0700

    added fonts for stylig

--- main.js
+++ main.js
@@ -50,4 +50,11 @@ $(document).ready(function(){
   contain: true
 });   
 
+// Fix "Skip Link" Focus in Webkit
+$(function() {
+   $("a[href^='#']").not("a[href='#']").click(function() {
+      $("#"+$(this).attr("href").slice(1)+"").focus();
+   });
+});
+
 
