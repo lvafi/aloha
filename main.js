1b414f5026dfd39730a4a904792d114103754f7f
vf <l_vafi@yahoo.com>
2017-06-07 23:52:35 -0700

    refactored javascript code for flickity by removing some of the unnecessary options

--- main.js
+++ main.js
@@ -1,50 +1,34 @@
-$(document).ready(function () {
-  $("a").on('click', function (event) {
-
-
-    if (this.hash !== "") {
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function () {
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
-
-
-//alert box for email list
-$('form button').on('click',
-  function (t) {
-    t.preventDefault();
-    var e = $("#your-email");
-    0 !== e.val().length ? (alert("Thanks for subscribing!"),
-      e.val("")) : alert("Please submit a valid email address.")
-  })
-
-//initialized Flickity
-$('.product-list').flickity({
-  cellAlign: 'left',
-  percentPosition: !1,
-  autoPlay: !0,
-  imagesLoaded: !0,
-  prevNextButtons: !1,
-  contain: !0,
-  contain: true
-});
-
-// Fix "Skip Link" Focus in Webkit
 $(function () {
-  $("a[href^='#']").not("a[href='#']").click(function () {
-    $("#" + $(this).attr("href").slice(1) + "").focus();
+  //initialized Flickity
+  $('.product-list').flickity({
+    cellAlign: 'left',
+    percentPosition: !1,
+    autoPlay: !0,
+    imagesLoaded: !0,
+    prevNextButtons: !1,
+    contain: !0,
+    contain: true
   });
+  //Smooth Scrolling
+  $('nav a[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+  //alert box for email list
+  $('form button').on('click',
+    function (t) {
+      t.preventDefault();
+      var e = $("#your-email");
+      0 !== e.val().length ? (alert("Thanks for subscribing!"),
+        e.val("")) : alert("Please submit a valid email address.")
+    })
+
 });
