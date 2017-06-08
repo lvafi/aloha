6e930b09b063473d1027ca75864011e1dc425bfe
vf <l_vafi@yahoo.com>
2017-06-07 22:45:22 -0700

    add screenshot to my readme file

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
 #project1
+![Aloha Screenshot](images/aloha-screenshot.png)
 This is a thoughtful write-up about my project...
 
+## Description
 - Create a one-page, responsive website that allows a user to find a path they need easily
 - Optimize for 3 widths: mobile, min-width: 600px, and min-width: 1240px.
 
 
-Technologies Used:
+##Technologies Used:
 
 - Google Chrome (https://www.google.com/chrome/)
 - Google Chrome Developer Tools
+- Flickity (http://flickity.metafizzy.co/)
+- Font Awesome (http://fontawesome.io/)
+- Font Squirrel (https://www.fontsquirrel.com/)
 - Eye Dropper (Google Chrome extension)
-- HTML Validator (https://validator.w3.org/nu/#file)
-- CSS Validator (https://jigsaw.w3.org/css-validator/#validate_by_upload)
-- JavaScript
+- W3C HTML Validator (https://validator.w3.org/nu/#file)
+- W3C Validation service (https://jigsaw.w3.org/css-validator/#validate_by_upload)
 - JQuery (https://jquery.com/)
+- Git
 
 
-Personal Learnings:
+## Personal Learnings:
+This project allowed me to utilize HTML, CSS and JavaScript to develop a website and 
+expand my knowledge in the following ways:
 
-- I am a visual learner and writing out the design of a site helps me.
+- The process of refactoring  my code to build a responsive website helped me understand the different properties one has to consider when using a mobile-first approach.
 
 - The value of div tools can be very helpful in identifying underlying issues in code or assist in making a website consistent across many browsers. 
 
--- main.js
+++ main.js
@@ -1,15 +1,7 @@
+$(document).ready(function () {
+  $("a").on('click', function (event) {
 
 
-
-
-
-
-
-  
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-
-    
     if (this.hash !== "") {
       event.preventDefault();
 
@@ -20,8 +12,8 @@ $(document).ready(function(){
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
+      }, 800, function () {
+
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
@@ -30,31 +22,29 @@ $(document).ready(function(){
 });
 
 
- 
- 
-     
-     
- $(".newsletter").on("submit","form",
- function(t){t.preventDefault();var e=$("#your-email");
- 0!==e.val().length?(alert("Thanks for subscribing!"),
- e.val("")):alert("Please submit a valid email address.")})
-    
 
- $('.product-list').flickity({
+$(".newsletter").on("submit", "form",
+  function (t) {
+    t.preventDefault();
+    var e = $("#your-email");
+    0 !== e.val().length ? (alert("Thanks for subscribing!"),
+      e.val("")) : alert("Please submit a valid email address.")
+  })
+
+
+$('.product-list').flickity({
   cellAlign: 'left',
-  percentPosition:!1,
-  autoPlay:!0,
-  imagesLoaded:!0,
-  prevNextButtons:!1,
-  contain:!0,
+  percentPosition: !1,
+  autoPlay: !0,
+  imagesLoaded: !0,
+  prevNextButtons: !1,
+  contain: !0,
   contain: true
-});   
-
-// Fix "Skip Link" Focus in Webkit
-$(function() {
-   $("a[href^='#']").not("a[href='#']").click(function() {
-      $("#"+$(this).attr("href").slice(1)+"").focus();
-   });
 });
 
-
+// Fix "Skip Link" Focus in Webkit
+$(function () {
+  $("a[href^='#']").not("a[href='#']").click(function () {
+    $("#" + $(this).attr("href").slice(1) + "").focus();
+  });
+});
